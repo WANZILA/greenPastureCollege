c6e6e88acdbd74c355a9902402e486ccc17fd8ed
Wanzila Ronny <wanzilro@gmail.com>
2020-08-09 14:47:58 +0300

    layouts on home and customizations

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 
 
 @NgModule({
-  declarations: [AboutUsComponent],
+  declarations: [
+    AboutUsComponent
+  ],
   imports: [
     CommonModule,
     AboutRoutingModule
