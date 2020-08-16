0c62adc81a338a5b347860dbf4d0f446d7c60733
Wanzila Ronny <wanzilro@gmail.com>
2020-08-16 11:06:41 +0300

    finised fees-payments on 16/aug/2020

--- src/app/admission/admission.module.ts
+++ src/app/admission/admission.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 // import { AdmissionRoutingModule } from './admission-routing.module';
 import { FeesTuitionPaymentsComponent } from './fees-tuition-payments/fees-tuition-payments.component';
 import { AdmissionRoutingModule } from './admission-routing.module';
+import { AdmissionRequirementsComponent } from './admission-requirements/admission-requirements.component';
 
 
 @NgModule({
-  declarations: [FeesTuitionPaymentsComponent],
+  declarations: [FeesTuitionPaymentsComponent, AdmissionRequirementsComponent],
   imports: [
     CommonModule,
     AdmissionRoutingModule
